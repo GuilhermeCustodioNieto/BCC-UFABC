b4864d6195201efb43bdab6c984ccd43b6067c7c
Guilherme Custodio Nieto <109774830+GuilhermeCustodioNieto@users.noreply.github.com>
2026-06-11 22:40:25 -0300

    feat: alguns exercícios de lógica de programação

---
+++ CelsiusToFarenheit.py
@@ -0,0 +1,10 @@
+C = 55
+F = C * (9/5) + 32
+
+print(f"{C} graus Celsius corresponde a {F} graus Fahrenheit")
+
+C = 65
+F = C * (9/5) + 32
+
+print(f"{C} graus Celsius corresponde a {F} graus Fahrenheit")
+
---
+++ logica-de-programacao/area_circulo.py
@@ -0,0 +1,4 @@
+import math
+raio = float(input())
+circunferencia = math.pi * (raio ** 2)
+print(f'{circunferencia:.4f}')
